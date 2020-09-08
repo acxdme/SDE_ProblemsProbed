de65ac7bd70695d347adcaea8c898b588db3444e
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-09-08 17:09:58 +0530

    Update 039-(Find starting point of the loop).cpp

--- 039-(Find starting point of the loop).cpp
+++ 039-(Find starting point of the loop).cpp
@@ -6,4 +6,31 @@ Algorithm : Detect if there exist a loop or not using hare and tortoise Algorith
             The position where the pointers meet is the starting point of the loop.
 */
 
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) 
+    {
+        if(head ==NULL or head->next==NULL)
+            return NULL;
+        ListNode* slow=head;
+        ListNode* fast= head;
+        do
+        {
+            if(fast==NULL or fast->next==NULL )
+                return NULL;
+            fast=fast->next->next;
+            slow=slow->next;
+            
+        }while(slow!=fast);
+        
+        slow=head;
+        while(fast!=slow)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
+
           
