ac19658ae196d80391772c1c0fe9478a2247bfa7
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-09-08 15:23:07 +0530

    Update 042-(RemoveDuplicateFromSortedArray).cpp

--- 042-(RemoveDuplicateFromSortedArray).cpp
+++ 042-(RemoveDuplicateFromSortedArray).cpp
@@ -2,9 +2,9 @@
 Time complexity : O(N)
 Space complexity : O(1)
 Algo :  Since the array is already sorted, we can keep two pointers ii and jj, where ii is the slow-runner while jj is the fast-runner.
-        As long as nums[i] = nums[j]nums[i]=nums[j], we increment jj to skip the duplicate.
-        When we encounter nums[j] \neq nums[i]nums[j]  = nums[i], the duplicate run has ended so we must copy its value to nums[i + 1]nums[i+1].
-        i is then incremented and we repeat the same process again until jj reaches the end of array.
+        As long as nums[ii] = nums[jj], we increment jj to skip the duplicate.
+        When we encounter nums[jj] ! =nums[ii], the duplicate run has ended so we must copy its value to nums[ii + 1].
+        ii is then incremented and we repeat the same process again until jj reaches the end of array.
 */
 
 class Solution {
