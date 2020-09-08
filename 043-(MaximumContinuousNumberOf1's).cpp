988780e17efb5b9b6c257f1208342c0fbfe47d3f
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-09-08 15:20:31 +0530

    Update 043-(MaximumContinuousNumberOf1's).cpp

--- 043-(MaximumContinuousNumberOf1's).cpp
+++ 043-(MaximumContinuousNumberOf1's).cpp
@@ -1,7 +1,9 @@
 /*
-Time complexity :
-Space complexity :
-Algo :
+Time complexity : O(N)
+Space complexity : O(1)
+Algo : Iterate over the array and increase the count if element is 1
+        store count in each iteration as  answer where answer= (answer, count)
+        if element is 0 reset count to 0.
 
 */
 
