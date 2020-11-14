083da74c5d642dbf86e0f149650c523a362b0b38
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-11-14 19:04:05 +0530

    Update 043-(MaximumContinuousNumberOf1's).cpp

--- 043-(MaximumContinuousNumberOf1's).cpp
+++ 043-(MaximumContinuousNumberOf1's).cpp
@@ -12,17 +12,15 @@ public:
     int findMaxConsecutiveOnes(vector<int>& nums) 
     {
         int n= nums.size();
-        if(n==0)
-            return 0;
-        int  count=0;
-        int answer=0;
+        if(n==0)  return 0;
+        int  count=0 , answer=0;
         for(int i=0 ;i<n;i++)
         {
             if(nums[i]==1)
                 count++;
             else
                 count=0;
-            answer= max(answer, count);  // 11110
+            answer= max(answer, count);   // 11110
          }
         
         return answer;
