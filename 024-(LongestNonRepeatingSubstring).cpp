5bb1da3b0d122db61532af2e4059c810a54ffc2b
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-11-14 10:22:01 +0530

    Update 024-(LongestNonRepeatingSubstring).cpp

--- 024-(LongestNonRepeatingSubstring).cpp
+++ 024-(LongestNonRepeatingSubstring).cpp
@@ -54,3 +54,28 @@ Method-2  the more optimal method , would be to store the character along with i
 Time complexity : O(N)
 Space complexity : O(N)
 */
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        unordered_map<char,int> ump;// (char,index)
+        int i=0 , j=0;
+        int res=0;
+        
+        while(j<n)
+        {
+            if(ump.find(s[j]) != ump.end() ) 
+            {
+                i = max(i,1+ ump[s[j]]); // update i to a valid position only when it lies between  left and  right (region on interest)
+            }
+       
+             ump[s[j]] = j;
+             res = max(res, j-i+1);
+             j++;
+        }
+       
+        
+        return res;
+    }
+};
