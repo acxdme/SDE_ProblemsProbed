1790626a1df8c941f88023434564214697bf0d34
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-11-14 12:45:52 +0530

    Update 040-(3SumOrTripletSum).cpp

--- 040-(3SumOrTripletSum).cpp
+++ 040-(3SumOrTripletSum).cpp
@@ -69,3 +69,43 @@ bool findTriplets(int arr[], int n)
     }
     return false;
 }
+	
+//3sum original question, the task is to find distinct triplets that add upto 0
+/*
+ Time complexity : O(N^2)
+ Space complexity : O(1)
+ */
+	
+class Solution {
+public:
+     vector<vector<int>> threeSum(vector<int>& nums)
+     {
+        vector<vector<int>> ans;
+         
+        if(nums.size()<=2)
+            return ans;
+        sort(nums.begin(),nums.end());
+         
+        for(int i=0;i<nums.size()-2;i++)
+        {
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            int l=i+1,r=nums.size()-1;
+            while(l<r)
+            {
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0)
+                {
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1])l++;
+                    while(l<r &&nums[r]==nums[r+1])r--;
+                }
+                else if(sum<0)l++;
+                else
+                    r--;
+            }
+        }
+        return ans;
+    } 
+};
